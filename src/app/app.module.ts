a98f628dda408fe33355c5ab755cbe0a2e3ba299
Fredrik Gullberg <frgul006@hotmail.com>
2018-12-18 20:02:19 +0100

    Fixed icons and graphql stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,29 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { TransferHttpCacheModule } from '@nguniversal/common';
+import { MarkdownModule } from 'ngx-markdown';
 import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 import { ComponentsModule } from './components/components.module';
 import { ContainersModule } from './containers/containers.module';
+import { GraphQLModule } from './graphql.module';
 import { PagesModule } from './pages/pages.module';
 import { ProjectsComponent } from './pages/projects/projects.component';
 import { StartComponent } from './pages/start/start.component';
-import { GraphQLModule } from './graphql.module';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule.withServerTransition({ appId: 'lgcab' }),
-    GraphQLModule, 
+    GraphQLModule,
     HttpClientModule,
     ComponentsModule,
     ContainersModule,
     PagesModule,
+    MarkdownModule.forRoot(),
     RouterModule.forRoot([
       { path: '', component: StartComponent, pathMatch: 'full' },
       { path: 'uppdrag', component: ProjectsComponent }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,13 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
-
 import { ComponentsModule } from '../components/components.module';
 import { ContainersModule } from '../containers/containers.module';
+import { MarkdownModule } from 'ngx-markdown';
+import { NgModule } from '@angular/core';
 import { ProjectsComponent } from './projects/projects.component';
 import { StartComponent } from './start/start.component';
 
 @NgModule({
   declarations: [StartComponent, ProjectsComponent],
-  imports: [CommonModule, ComponentsModule, ContainersModule]
+  imports: [CommonModule, ComponentsModule, ContainersModule, MarkdownModule.forChild()]
 })
 export class PagesModule {}
