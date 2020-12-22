503137e2c837acd76bbc9103efce4540f07cb015
Fredrik Gullberg <frgul006@hotmail.com>
2020-12-22 21:17:38 +0100

    Added target=_blank to external links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { TransferHttpCacheModule } from '@nguniversal/common';
-import { MarkdownModule } from 'ngx-markdown';
+import { MarkdownModule, MarkdownModuleConfig, MarkedOptions, MarkedRenderer } from 'ngx-markdown';
 import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 import { ComponentsModule } from './components/components.module';
@@ -13,6 +13,25 @@ import { GraphQLModule } from './graphql.module';
 import { PagesModule } from './pages/pages.module';
 import { ProjectsComponent } from './pages/projects/projects.component';
 import { StartComponent } from './pages/start/start.component';
+
+export function markedOptionsFactory(): MarkedOptions {
+  const renderer = new MarkedRenderer();
+  const linkRenderer = renderer.link;
+
+  renderer.link = (href, title, text) => {
+    const html = linkRenderer.call(renderer, href, title, text);
+    return html.replace(/^<a /, '<a target="_blank" ');
+  };
+
+  return {
+    renderer,
+    gfm: true,
+    breaks: false,
+    pedantic: false,
+    smartLists: true,
+    smartypants: false
+  };
+}
 
 @NgModule({
   declarations: [AppComponent],
@@ -23,7 +42,12 @@ import { StartComponent } from './pages/start/start.component';
     ComponentsModule,
     ContainersModule,
     PagesModule,
-    MarkdownModule.forRoot(),
+    MarkdownModule.forRoot({
+      markedOptions: {
+        provide: MarkedOptions,
+        useFactory: markedOptionsFactory
+      }
+    }),
     RouterModule.forRoot([
       { path: '', component: StartComponent, pathMatch: 'full' },
       { path: 'uppdrag', component: ProjectsComponent }
